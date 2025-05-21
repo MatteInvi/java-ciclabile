9527a4c03d6d9a230eeadedde40320e258ca082e
MatteInvi <matteoinvidia@gmail.com>
2025-05-21 19:17:14 +0200

    formatting

--- src/org/excercise/java/ciclabile/IntegerClass.java
+++ src/org/excercise/java/ciclabile/IntegerClass.java
@@ -2,27 +2,24 @@ package org.excercise.java.ciclabile;
 
 public class IntegerClass {
 
- public int[] interi = {};
+    public int[] interi = {};
 
-
-    public IntegerClass(int[] interi){
+    public IntegerClass(int[] interi) {
         this.interi = interi;
     }
 
+    public int getElementoSuccessivo(int indice) {
 
-    public int getElementoSuccessivo(int indice){
-
-         return interi[indice]; 
+        return interi[indice];
     }
 
-    public boolean hasAncoraElementi(int indice, int lunghezzaArray){
+    public boolean hasAncoraElementi(int indice, int lunghezzaArray) {
         boolean risultato = true;
-        if (indice == lunghezzaArray - 1 ){
+        if (indice == lunghezzaArray - 1) {
             risultato = false;
         }
         return risultato;
     }
-
 
     public int[] getInteri() {
         return this.interi;
--- src/org/excercise/java/ciclabile/Main.java
+++ src/org/excercise/java/ciclabile/Main.java
@@ -1,28 +1,20 @@
 package org.excercise.java.ciclabile;
+
 public class Main {
 
-   
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        int[] mynumber = {23,324,234,23453,877};
+    int[] mynumber = { 23, 324, 234, 23453, 877 };
 
-        IntegerClass numeri = new IntegerClass(mynumber);
+    IntegerClass numeri = new IntegerClass(mynumber);
 
-        ;
+    for (int i = 0; i < numeri.interi.length; i++) {
 
-       
-       
+      System.out.println(numeri.getElementoSuccessivo(i));
+      System.out.println(numeri.hasAncoraElementi(i, numeri.interi.length));
 
-         for (int i = 0; i < numeri.interi.length; i++){
-
-           System.out.println(numeri.getElementoSuccessivo(i));
-           System.out.println(numeri.hasAncoraElementi(i, numeri.interi.length));
-
-         }
-           
-        
-        
-    
     }
+
+  }
 
 }
