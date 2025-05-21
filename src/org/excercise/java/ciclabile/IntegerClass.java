9e29b3168a34b7aa1e89a0ac19b0073bdb9af67e
MatteInvi <matteoinvidia@gmail.com>
2025-05-21 19:16:44 +0200

    first commit

---
+++ src/org/excercise/java/ciclabile/IntegerClass.java
@@ -0,0 +1,35 @@
+package org.excercise.java.ciclabile;
+
+public class IntegerClass {
+
+ public int[] interi = {};
+
+
+    public IntegerClass(int[] interi){
+        this.interi = interi;
+    }
+
+
+    public int getElementoSuccessivo(int indice){
+
+         return interi[indice]; 
+    }
+
+    public boolean hasAncoraElementi(int indice, int lunghezzaArray){
+        boolean risultato = true;
+        if (indice == lunghezzaArray - 1 ){
+            risultato = false;
+        }
+        return risultato;
+    }
+
+
+    public int[] getInteri() {
+        return this.interi;
+    }
+
+    public void setInteri(int[] interi) {
+        this.interi = interi;
+    }
+
+}
---
+++ src/org/excercise/java/ciclabile/Main.java
@@ -0,0 +1,28 @@
+package org.excercise.java.ciclabile;
+public class Main {
+
+   
+    public static void main(String[] args) {
+
+        int[] mynumber = {23,324,234,23453,877};
+
+        IntegerClass numeri = new IntegerClass(mynumber);
+
+        ;
+
+       
+       
+
+         for (int i = 0; i < numeri.interi.length; i++){
+
+           System.out.println(numeri.getElementoSuccessivo(i));
+           System.out.println(numeri.hasAncoraElementi(i, numeri.interi.length));
+
+         }
+           
+        
+        
+    
+    }
+
+}
